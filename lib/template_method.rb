6590c37b92869f8c46677a78f189d2c0dc19c5aa
afurm <furmanets.andriy@gmail.com>
2014-01-31 18:23:22 +0200

    Template Method Pattern

--- lib/template_method.rb
+++ lib/template_method.rb
@@ -1,11 +1,65 @@
 class Hero
-  attr_reader :damage
+  attr_reader :damage, :abilities
 
   def initialize
-    @damage = 10
+    initialize_stats
+
+    @damage = damage_rating
+    @abilities = occupation_abilities
   end
 
   def attack
     "Attacked dealing #{@damage} damage"
+  end
+
+  def greet
+    greeting = ["Hello"]
+    greeting << uniq_greeting_line
+    greeting
+  end
+
+  def uniq_greeting_line
+    raise "You must define uniq_greeting_line"
+  end
+
+  def damage_rating
+    10
+  end
+
+  def occupation_abilities
+    []
+  end
+
+  def initialize_stats
+  end
+end
+
+class Warrior < Hero
+
+  def damage_rating
+    15
+  end
+
+  def occupation_abilities
+    [:strike]
+  end
+
+  def uniq_greeting_line
+    "Warrior is ready to fight"
+  end
+end
+
+class Mage < Hero
+
+  def damage_rating
+    7
+  end
+
+  def occupation_abilities
+    [:magic_arrow]
+  end
+
+  def uniq_greeting_line
+    "Mage is ready to fight"
   end
 end
--- spec/template_method_spec.rb
+++ spec/template_method_spec.rb
@@ -2,13 +2,48 @@ require 'spec_helper'
 require_relative '../lib/template_method.rb'
 
 describe 'Template Method Pattern' do
-  let(:hero) { Hero.new }
 
-  it 'has default damage rating of 10' do
-    expect(hero.damage).to eq(10)
+  context 'Default Hero' do
+    let(:hero) { Hero.new }
+    it 'has default damage rating of 10' do
+      expect(hero.damage).to eq(10)
+    end
+
+    it 'can attack' do
+      expect(hero.attack).to eq("Attacked dealing 10 damage")
+    end
+
+    it 'requires subclasses to implement uniq_greeting_line' do
+      expect{hero.greet}.to raise_exception
+    end
   end
 
-  it 'can attack' do
-    expect(hero.attack).to eq("Attacked dealing 10 damage")
+  context 'Warrior' do
+    let(:hero) { Warrior.new }
+    it 'has default damage rating of 15' do
+      expect(hero.damage).to eq(15)
+    end
+
+    it 'has Strike ability' do
+      expect(hero.abilities).to include(:strike)
+    end
+
+    it 'greets other character' do
+      expect(hero.greet).to eq(["Hello", "Warrior is ready to fight"])
+    end
+  end
+
+  context 'Mage' do
+    let(:hero) { Mage.new }
+    it 'has default damage rating of 7' do
+      expect(hero.damage).to eq(7)
+    end
+
+    it 'has Mage ability' do
+      expect(hero.abilities).to include(:magic_arrow)
+    end
+    it 'greets other character' do
+      expect(hero.greet).to eq(["Hello", "Mage is ready to fight"])
+    end
   end
 end
