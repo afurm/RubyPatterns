b2ca3f0d7b14d10775d97c475f89a3ae3d045a5f
afurm <furmanets.andriy@gmail.com>
2014-01-31 17:37:37 +0200

    Basic Hero class

---
+++ spec/template_method_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require_relative '../lib/template_method.rb'
+
+describe 'Template Method Pattern' do
+  let(:hero) { Hero.new }
+
+  it 'has default damage rating of 10' do
+    expect(hero.damage).to eq(10)
+  end
+
+  it 'can attack' do
+    expect(hero.attack).to eq("Attacked dealing 10 damage")
+  end
+end
